446901dd2e15f22a3581294f518f2f94d14e2af6
Mohit Negi <mnegi1433@gmail.com>
2024-11-20 02:36:05 +0530

    Dep issues

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,26 +1,26 @@
-// import { prisma } from "@/prisma";
-// import bcrypt from "bcrypt";
+import { prisma } from "@/prisma";
+import bcrypt from "bcrypt";
 
-// export const POST = async (req:Request) => {
-//     try {
-//         const body = await req.json();
-//         const emailExist = await prisma.user.findUnique({
-//             where: {
-//                 email: body.email
-//             }
-//         })
+export const POST = async (req:Request) => {
+    try {
+        const body = await req.json();
+        const emailExist = await prisma.user.findUnique({
+            where: {
+                email: body.email
+            }
+        })
 
-//         if (!!emailExist) {
-//             return Response.json({ message: "User with given email already exist", status: 409 });
-//         }
+        if (!!emailExist) {
+            return Response.json({ message: "User with given email already exist", status: 409 });
+        }
         
-//         const hashedPassword = await bcrypt.hash(body.password, 10);
-//         await prisma.user.create(
-//             {...body, password:hashedPassword}
-//         )
+        const hashedPassword = await bcrypt.hash(body.password, 10);
+        await prisma.user.create(
+            {...body, password:hashedPassword}
+        )
         
-//         return Response.json({ message: "User created successfully", status: 200 });
-//     } catch (error) {
-//         return Response.json({ message: "Internal server error", status: 500 });
-//     }
-// }
+        return Response.json({ message: "User created successfully", status: 200 });
+    } catch (error) {
+        return Response.json({ message: "Internal server error", status: 500 });
+    }
+}
